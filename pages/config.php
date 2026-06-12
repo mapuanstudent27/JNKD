02703fb84808220eff4c0f79b40709babf0514cb
mapuanstudent27 <tuazonjoshua025@gmail.com>
2026-06-12 13:46:06 +0800

    Update database connection details and add port

--- pages/config.php
+++ pages/config.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost"; 
-$user = "root"; 
-$password = ""; 
-$database = "users_db"; 
+$host = "mysql-2378d1c4-jnkd-project.g.aivencloud.com"; 
+$user = "avnadmin"; 
+$password = "AVNS_rhKkoHP4WJ927TZL5Mr"; 
+$database = "defaultdb"; 
+$port = 14355;
 
-$con = new mysqli($host, $user, $password, $database);
+$con = new mysqli($host, $user, $password, $database, $port);
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
